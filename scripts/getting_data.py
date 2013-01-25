4b3f1a19d68758c41e9195624c0c45133542a505
Tomas Dalmo <tomasdalmo@thomasmba.scilifelab.se>
2013-01-25 15:11:19 +0100

    script

--- scripts/getting_data.py
+++ scripts/getting_data.py
@@ -4,17 +4,21 @@ import untangle
 EscStat =  urllib2.urlopen('http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml')
 html = EscStat.read()
 
-doc = untangle.parse(html)
-outages = doc.NYCOutages.outage
+def CountRepair(xml):
+    doc = untangle.parse(xml)
+    outages = doc.NYCOutages.outage
 
-r=0
-n=0
+    r=0
+    n=0
 
-for f in range(len(outages)):
-    if outages[f].reason.cdata=='REPAIR':
-        r+=1
-    else:
-        n+=1
+    for f in range(len(outages)):
+        if outages[f].reason.cdata=='REPAIR':
+            r+=1
+        else:
+            n+=1
 
-print r,"of",r+n,"outages has the reason repair"
+    print r,"of",r+n,"outages has the reason repair"
 
+    return
+
+CountRepair(html)
