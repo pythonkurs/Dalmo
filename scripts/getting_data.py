6743aa9f81649ef72746ea94c60be45f8a862f82
Tomas Dalmo <tomasdalmo@thomasmba.scilifelab.se>
2013-01-25 15:14:41 +0100

    script

--- scripts/getting_data.py
+++ scripts/getting_data.py
@@ -1,8 +1,13 @@
 import urllib2
 import untangle
 
+#Opens the xml-file from the url and reads it using urllib2
 EscStat =  urllib2.urlopen('http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml')
 html = EscStat.read()
+
+#The definition uses untangle to pars the xml-file
+#It goes thorugh all the outages in NYC and counts the 
+#ones due to repair.
 
 def CountRepair(xml):
     doc = untangle.parse(xml)
